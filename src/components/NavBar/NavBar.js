a2bafcc0bca0a4e98733120499f18a1ae1e2fb0e
Jacob Adamson <jakeladamson@gmail.com>
2021-09-28 20:06:11 -0600

    cleaned up unused code, fixed margin on icon

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,6 +1,5 @@
 import React from "react";
 // import Navbar from 'react-bootstrap/Button';
-// import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
 import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
@@ -11,8 +10,7 @@ function NavBar() {
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
-        {/* <img src="./web-dev.PNG" alt="icon"></img> */}
-        <Image src={Picture} />
+        <Image src={Picture} className="me-3" />
         <Navbar.Brand href="#home">Jacob L. Adamson</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
@@ -20,22 +18,14 @@ function NavBar() {
             <Nav.Link href="#">About Me</Nav.Link>
             <Nav.Link href="#">Portfolio</Nav.Link>
             <Nav.Link href="#">Contact</Nav.Link>
-            <Nav.Link href="../../../public/images/JacobAdamsonResume2021.pdf">
+            <Nav.Link
+              href="https://drive.google.com/file/d/1aIVjM-gp8WeiJ6XnyC0h7W71RUcmoXsc/view?usp=sharing"
+              target="_blank"
+            >
               Resume
             </Nav.Link>
-            {/* <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown> */}
           </Nav>
-          <Nav>
+          {/* <Nav>
             <Nav.Link href="https://github.com/JKLA13" target="_blank">
               Github
             </Nav.Link>
@@ -46,7 +36,7 @@ function NavBar() {
             >
               LinkedIn
             </Nav.Link>
-          </Nav>
+          </Nav> */}
         </Navbar.Collapse>
       </Container>
     </Navbar>
