a23881a8e53b648da9529d6e8e5ff4ac396317f3
Jacob Adamson <jakeladamson@gmail.com>
2021-09-28 22:53:02 -0600

    coded skeleton for folio section

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,30 +5,54 @@ import Col from "react-bootstrap/Col";
 import Picture from "../../assets/images/folio-pic.PNG";
 import Image from "react-bootstrap/Image";
 // import React, { component } from "react";
+import Card from "react-bootstrap/Card";
 
 function Home() {
   return (
-    <Container>
-      <Row>
-        <Col sm={1}>About Me</Col>
-        <Col sm={3}>
-          <Image src={Picture} className="img-fluid" />
-        </Col>
-        <Col sm={8}>
-          <p>
-            Hello, I'm Jacob L. Adamson. I am a Full Stack Web Developer (MERN),
-            and I am beyond excited to keep coding and learning. My other
-            passions are writing and recording my music, as well as recording
-            other's music.{" "}
-          </p>
-        </Col>
-      </Row>
-      {/* <Row>
-          <Col sm>sm=true</Col>
-          <Col sm>sm=true</Col>
-          <Col sm>sm=true</Col>
-        </Row> */}
-    </Container>
+    //   about me section
+    <>
+      <Container>
+        <Row>
+          <Col sm={1}>About Me</Col>
+          <Col sm={3}>
+            <Image src={Picture} className="img-fluid" />
+          </Col>
+          <Col sm={8}>
+            <p>
+              Hello, I'm Jacob L. Adamson. I am a Full Stack Web Developer
+              (MERN), and I am beyond excited to keep coding and learning. My
+              other passions are writing and recording my music, as well as
+              recording other's music.{" "}
+            </p>
+          </Col>
+        </Row>
+      </Container>
+      {/* // Portfolio section */}
+      <Container>
+        <Row>
+          <Col sm={1}>Portfolio</Col>
+          <Col sm={11}>
+            <Row xs={1} md={2} className="g-4">
+              {Array.from({ length: 6 }).map((_, idx) => (
+                <Col>
+                  <Card>
+                    <Card.Img variant="top" src="holder.js/100px160" />
+                    <Card.Body>
+                      <Card.Title>Card title</Card.Title>
+                      <Card.Text>
+                        This is a longer card with supporting text below as a
+                        natural lead-in to additional content. This content is a
+                        little bit longer.
+                      </Card.Text>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              ))}
+            </Row>
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 }
 
