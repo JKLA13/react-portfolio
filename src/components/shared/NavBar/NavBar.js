142dc1ac0a597c53d560ea883c4252724502dba0
Jacob Adamson <jakeladamson0@gmail.com>
2022-04-19 20:57:24 -0600

    update resume pdf

--- src/components/shared/NavBar/NavBar.js
+++ src/components/shared/NavBar/NavBar.js
@@ -18,7 +18,7 @@ function NavBar() {
             <Nav.Link href="#Portfolio">Portfolio</Nav.Link>
             <Nav.Link href="#Contact">Contact</Nav.Link>
             <Nav.Link
-              href="https://docs.google.com/document/d/1HP2WiuwLLEnoOIeJCSxwgZzvDZ7aakqeQGVOp6vhe7U/edit?usp=sharing"
+              href="https://drive.google.com/file/d/1Ew_JgaiBLqa5KCarIuAhsYU_qKK0zBd8/view?usp=sharing"
               target="_blank"
             >
               Resume
